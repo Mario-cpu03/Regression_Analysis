464b736f91ded9d06c1960f7d776813de6a17142
Mario Pellegrino Ambrosone <m.ambrosone12@studenti.unisa.it>
2025-06-14 18:48:46 +0200

    START REGRESSION

--- src/regressionModel.R
+++ src/regressionModel.R
@@ -1,4 +1,12 @@
 ### Regression Model Script
 
-# The regressionModel script is used to define
-# and hyopotize different 
+# The regressionModel script is used to define and hypothesize 
+# different regression models to evaluate the relationship 
+# between the dependent and independent variables that have been
+# identified in the descriptiveAnalysis script.
+
+# A first evaluation of the relationship in terms of correlation 
+# between y_VideoQuality and xn_* (where n = 1,..,7) will help us 
+# define a number of potential models
+
+# COMPLETE MULTIPLE REGRESSION MODEL
